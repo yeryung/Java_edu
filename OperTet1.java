1c7791f0727c083ac5974120871df368bb4787e7
yeryung <68985912+yeryung@users.noreply.github.com>
2023-01-12 13:52:32 +0900

    commit java file

---
+++ OperTet1.java
@@ -0,0 +1,14 @@
+
+public class OperTet1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10, m = 10;
+		int bn = ++n;
+		int am = m++;
+		System.out.println(bn);
+		System.out.println(am);
+		
+	}
+
+}
